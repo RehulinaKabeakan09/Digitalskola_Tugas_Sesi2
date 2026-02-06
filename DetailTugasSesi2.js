987bf25ac52918bc6ab154bccf4f3d4674a4a7ce
Rehulina Kabeakan <rehulinakabeakan@gmail.com>
2026-02-06 10:42:42 +0700

    added DetailTugasSesi2.js

---
+++ DetailTugasSesi2.js
@@ -0,0 +1,15 @@
+"Tugas Sesi 2"
+
+// 1. Buatlah Buatlah testcase dan bug report pada website http://belajar-bareng.onrender.com/ (Scope: Login, dan Add User)
+// 2. Buatlah test planning pada website website http://belajar-bareng.onrender.com/ (Scope: Login, dan Add User)
+
+
+"Requirment"
+// Requirement Fungsi Add User:
+// 1. Username dan Age mandatory Field
+// 2. Maksimal panjang karakter dari Username adalah 10 karakter
+
+
+// Requirement Fungsi Login:
+// 1. Username dan Password mandatory Field
+// 2Jika input salah Password muncul error message
